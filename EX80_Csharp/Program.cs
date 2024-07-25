22b183e46c03d3e897e4f864ff206364766c2e19
ChuManhCuong312 <146830932+ChuManhCuong312@users.noreply.github.com>
2024-07-25 15:55:04 +0700

    Add files via upload

---
+++ EX80_Csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static Random random = new Random();
+
+    static void ff()
+    {
+        while (true)
+        {
+            int n = random.Next();
+            if (n % 2025 == 0)
+            {
+                break;
+            }
+            Console.WriteLine(n);
+            Thread.Sleep(100);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(new ThreadStart(ff));
+        thread.Start();
+        thread.Join();  
+        Console.WriteLine("Đã thực hiện xong.");
+    }
+}
